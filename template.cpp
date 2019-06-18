fd3c250e60f60b257f2274571894ee74a2c1f13f
ydk1727 <ydk_nk@outlook.jp>
2019-06-19 00:18:59 +0900

    fix

--- template.cpp
+++ template.cpp
@@ -1,5 +1,5 @@
 #include "bits/stdc++.h"
-// Custom Header <<<
+// Custom Header >>>
 #define ALL(x) x.begin(), x.end()
 #define rep(i, s, n)      for(int i(s); i < int(n); ++i)
 #define endl '\n'
@@ -11,10 +11,13 @@ using i64 = long long;
 using pii = pair<i64, i64>;
 template<class A, class B>inline bool chmax(A &a, const B &b){return b>a ? a=b,1 : 0;}
 template<class A, class B>inline bool chmin(A &a, const B &b){return b<a ? a=b,1 : 0;}
-constexpr int INF  = 0x3f3f3f3f;
-constexpr i64 LINF = 0x3f3f3f3f3f3f3f3fLL;
-constexpr int MOD  = int(1e9) + 7;
-// >>>
+constexpr const int INF  = 0x3f3f3f3f;
+constexpr const i64 LINF = 0x3f3f3f3f3f3f3f3fLL;
+constexpr const int MOD  = int(1e9) + 7;
+// <<<
+
+// Library >>>
+// <<<
 
 
 signed main()
@@ -22,4 +25,4 @@ signed main()
     return 0;
 }
 
-/* vim:set foldmethod=marker foldmarker=<<<,>>> : */
+/* vim:set foldmethod=marker foldmarker=>>>,<<< : */
