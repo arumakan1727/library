e63466e33f7248214b3cd977e993bdfb255eedfd
ydk1727 <ydk_nk@outlook.jp>
2019-06-21 22:08:34 +0900

    fix

--- template.cpp
+++ template.cpp
@@ -1,5 +1,5 @@
 #include "bits/stdc++.h"
-// Custom Header >>>
+// Custom Header {{{
 #define ALL(x) x.begin(), x.end()
 #define rep(i, s, n)      for(int i(s); i < int(n); ++i)
 #define endl '\n'
@@ -14,15 +14,13 @@ template<class A, class B>inline bool chmin(A &a, const B &b){return b<a ? a=b,1
 constexpr const int INF  = 0x3f3f3f3f;
 constexpr const i64 LINF = 0x3f3f3f3f3f3f3f3fLL;
 constexpr const int MOD  = int(1e9) + 7;
-// <<<
+// }}}
 
-// Library >>>
-// <<<
+// Library {{{
+// }}}
 
 
 signed main()
 {
     return 0;
 }
-
-/* vim:set foldmethod=marker foldmarker=>>>,<<< : */
