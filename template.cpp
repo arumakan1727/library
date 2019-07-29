b3a63ca64ce52ff44429a2a5af98cb128162ad33
ydk1727 <ydk_nk@outlook.jp>
2019-07-29 22:41:06 +0900

    fix fraction, template

--- template.cpp
+++ template.cpp
@@ -1,12 +1,13 @@
 #include "bits/stdc++.h"
 // Custom Header {{{
 #define ALL(x)      x.begin(), x.end()
-#define rep(i,n)    for (int i = 0; i < (n); ++i)
+#define rep(i,n)    for (i64 i = 0; i < (n); ++i)
+#define reps(i,s,t) for (i64 i = (s); i <= (t); ++i)
 using namespace std;
 using i64 = long long;
 using pii = pair<i64, i64>;
-template<class A, class B>inline bool chmax(A &a, const B &b){return b>a ? a=b,1 : 0;}
-template<class A, class B>inline bool chmin(A &a, const B &b){return b<a ? a=b,1 : 0;}
+template<class A, class B>inline bool chmax(A &a, const B &b){return b>a ? (a=b,1) : 0;}
+template<class A, class B>inline bool chmin(A &a, const B &b){return b<a ? (a=b,1) : 0;}
 constexpr int INF  = 0x3f3f3f3f;
 constexpr i64 LINF = 0x3f3f3f3f3f3f3f3fLL;
 // }}}
