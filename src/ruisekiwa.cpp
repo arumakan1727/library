3e03163d3099c89b763cf7d14264d976d55d02d8
ydk1727 <ydk_nk@outlook.jp>
2019-08-17 22:40:06 +0900

    fix ruisekiwa

--- src/ruisekiwa.cpp
+++ src/ruisekiwa.cpp
@@ -5,26 +5,68 @@
 //@ snippet ruisekiwa
 //@ alias   partSum
 //@ options head
-class Ruisekiwa : public vector<i64> { // {{{
-    using super = vector<i64>;
-    using super::super; // コンストラクタを継承
+class Ruisekiwa { // {{{{
+    vector<i64> cum;
 
 public:
+    Ruisekiwa() : cum(1) {}
+
+    Ruisekiwa(int sz) : cum(sz + 1) {}
+
+    template<class T>
+        Ruisekiwa(initializer_list<T> il) { assign(il.begin(), il.end()); }
+
+    template<class Iterator>
+        Ruisekiwa(Iterator begin, Iterator end) { assign(begin, end); }
+
+    template<class Iterator>
+        inline void assign(Iterator begin, Iterator end) {
+            cum.assign(distance(begin, end) + 1, 0);
+            std::copy(begin, end, cum.begin() + 1);
+        }
+
     inline void build() {
-        const auto sz = super::size();
-        for (int i = 1; i < sz; ++i) (*this)[i] += (*this)[i-1];
+        for (int i = 1; i < cum.size(); ++i) {
+            cum[i] += cum[i-1];
+        }
     }
 
+    // "半開区間" [0, r) までの総和
     inline i64 sum(int r) const {
-        return (r < 0) ? 0 : (*this)[r];
+        return cum[r];
     }
 
+    // "半開区間" [l, r) までの総和
     inline i64 sum(int l, int r) const {
-        return sum(r) - sum(l - 1);
+        return sum(r) - sum(l);
     }
 
-    inline int lower_bound(i64 key) const {
-        return std::lower_bound(super::begin(), super::end(), key) - super::begin();
+    ///////////////////////////////////////////////////////////////////////////
+    // vector like methods
+    inline void resize(int sz) {
+        cum.resize(sz + 1);
+    }
+    template<class... Args>
+        inline void emplace_back(Args&&... args) {
+            cum.emplace_back(args...);
+        }
+    inline i64 size() const noexcept {
+        return static_cast<i64>(cum.size()) - 1;
+    }
+    inline const auto& data() const noexcept {
+        return cum;
+    }
+    inline auto begin() const noexcept {
+        return cum.begin() + 1;
+    }
+    inline auto end() const noexcept {
+        return cum.end();
+    }
+    inline i64 operator[](int i) const {
+        return cum[i + 1];
+    }
+    inline i64& operator[](int i) {
+        return cum[i + 1];
     }
 };
 // }}}
@@ -34,115 +76,178 @@ public:
 //@ snippet ruisekiwa2d
 //@ options head
 class Ruisekiwa2D { // {{{
-    vector<vector<i64>> s;
+    vector<vector<i64>> cum;
 public:
     Ruisekiwa2D() {}
-    Ruisekiwa2D(int h, int w) : s(h+1, vector<i64>(w+1, 0)) {}
+    Ruisekiwa2D(int h, int w) : cum(h+1, vector<i64>(w+1, 0)) {}
 
     inline void build() {
-        const int h = s.size();
-        const int w = s[0].size();
-        for (int j = 1; j < w; ++j) {
-            s[0][j] += s[0][j-1];
-        }
-        for (int i = 1; i < h; ++i) {
-            s[i][0] += s[i-1][0];
-            for (int j = 1; j < w; ++j) {
-                s[i][j] += s[i-1][j] + s[i][j-1] - s[i-1][j-1];
+        const auto h = cum.size(),  w = cum[0].size();
+        for (int y = 1; y < h; ++y) {
+            for (int x = 1; x < w; ++x) {
+                cum[y][x] += cum[y][x - 1] + cum[y - 1][x] - cum[y - 1][x - 1];
             }
         }
-        return;
+    }
+
+    // Usage: sum({sy, sx}, {gy, gx})
+    inline i64 sum(pair<int, int> s, pair<int, int> t) const {
+        return sum(s.first, s.second, (t.first - s.first), (t.second - s.second));
+    }
+
+    inline i64 sum(int sy, int sx, int h, int w) const {
+        return cum[sy + h][sx + w] - cum[sy][sx + w] - cum[sy + h][sx] + cum[sy][sx];
     }
 
     inline i64 at(int y, int x) const {
-        return (y < 0) || (x < 0) ? 0 : s[y][x];
+        return cum[y + 1][x + 1];
     }
 
-    inline i64 sum(int sy, int sx, int gy, int gx) const {
-        return s[gy][gx] - at(sy-1, gx) - at(gy, sx-1) + at(sy-1, sx-1);
+    inline i64& at(int y, int x) {
+        return cum[y + 1][x + 1];
     }
 
-    inline void resize(int h, int w) { s.assign(h+1, vector<i64>(w+1, 0)); }
-    inline const vector<i64>& operator[] (size_t i) const { return s[i]; }
-    inline vector<i64>&       operator[] (size_t i)       { return s[i]; }
+    inline void resize(int h, int w) {
+        cum.assign(h+1, vector<i64>(w+1, 0));
+    }
+
+    inline i64 height() const noexcept {
+        return cum.size() - 1;
+    }
+
+    inline i64 width() const noexcept {
+        return cum[0].size() - 1;
+    }
+
+    friend ostream& operator<< (ostream &out, const Ruisekiwa2D &mat) {
+        const auto h = mat.cum.size();
+        const auto w = mat.cum[0].size();
+        for (int i = 1; i < h; ++i) {
+            for (int j = 1; j < w; ++j) {
+                out << mat.cum[i][j] << " \n"[j + 1 == w && i + 1 < h];
+            }
+        }
+        return out;
+    }
+
 }; // }}}
 //@@@@@@@@@@@
 
-
-/*
- * Verified On : 2019-07-22
- * https://atcoder.jp/contests/abc005/tasks/abc005_4
- */
-// Ruisekiwa2D - Verify {{{
-void ABC005_D()
+void test_Ruisekiwa1D() // {{{
 {
-    using i64 = long long;
+    vector<i64> src {{ 3, 2, 0, 4 }};
+    Ruisekiwa cum(src.begin(), src.end());
 
-    int N; cin >> N;
+    cum.build();
 
-    Ruisekiwa2D mat(N, N);
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            cin >> mat[i][j];
-        }
-    }
+    outl(src);
+    outl(cum.data());
 
-    mat.build();
+    assert(cum.size() == 4);
+    assert(cum[0] == 3);
+    assert(cum[1] == 5);
+    assert(cum[2] == 5);
+    assert(cum[3] == 9);
+    assert(cum.sum(0) == 0);
+    assert(cum.sum(1) == 3);
+    assert(cum.sum(2) == 5);
+    assert(cum.sum(3) == 5);
+    assert(cum.sum(4) == 9);
+    assert(cum.sum(0, 1) == 3);
+    assert(cum.sum(0, 4) == 9);
+    assert(cum.sum(2, 3) == 0);
 
-    vector<i64> ans(N*N + 10, 0);
-    reps(h, 1, N) reps(w, 1, N) {
-        for (int i = 0; i+h <= N; ++i) {
-            for (int j = 0; j+w <= N; ++j) {
-                chmax(ans[h*w], mat.sum(i,j, i+h-1, j+w-1));
-            }
-        }
-    }
-    for (int i = 1; i < ans.size(); ++i) {
-        chmax(ans[i], ans[i-1]);
-    }
 
-    int Q; cin >> Q;
-    while (Q--) {
-        int p; cin >> p;
-        cout << ans[p] << "\n";
-    }
-}
-// }}}
+    cum.resize(0);
+    assert(cum.size() == 0);
+    assert(cum.data().size() == 1);
+
+
+    cum.emplace_back(0);
+    cum.emplace_back(10);
+    cum.build();
+    assert(cum.size() == 2);
+    assert(cum.sum(0) == 0);
+    assert(cum.sum(1) == 0);
+    assert(cum.sum(2) == 10);
+
+
+    cum = Ruisekiwa(2);
+    assert(cum.size() == 2);
+    assert(cum.sum(0) == 0);
+    assert(cum.sum(1) == 0);
+    assert(cum.sum(2) == 0);
+
+    outl(__func__, ": AC");
+} // }}}
 
 void test_Ruisekiwa2D() // {{{
 {
     Ruisekiwa2D mat(2, 2);
-    mat[0][0] = 2;  mat[0][1] = 1;
-    mat[1][0] = 3;  mat[1][1] = 5;
+    mat.at(0, 0) = 2;  mat.at(0, 1) = 1;
+    mat.at(1, 0) = 3;  mat.at(1, 1) = 5;
+    assert(mat.height() == 2);
+    assert(mat.width() == 2);
+
+    outl("--------");
+    outl(mat); // before
 
     mat.build();
 
-    assert(mat[0][0] == 2); assert(mat[0][1] == 3);
-    assert(mat[1][0] == 5); assert(mat[1][1] == 11);
+    outl("--------");
+    outl(mat); // after
 
-    assert(mat.sum(0,0, 0,0) == 2);
-    assert(mat.sum(0,0, 1,0) == 5);
-    assert(mat.sum(0,0, 0,1) == 3);
-    assert(mat.sum(0,0, 1,1) == 11);
+    assert(mat.at(0, 0) == 2); assert(mat.at(0, 1) == 3);
+    assert(mat.at(1, 0) == 5); assert(mat.at(1, 1) == 11);
 
-    assert(mat.sum(0,1, 0,1) == 1);
-    assert(mat.sum(0,1, 1,1) == 6);
+    assert(mat.sum(0,0, 1,1) == 2);
+    assert(mat.sum(0,0, 2,1) == 5);
+    assert(mat.sum(0,0, 1,2) == 3);
+    assert(mat.sum(0,0, 2,2) == 11);
 
-    assert(mat.sum(1,0, 1,0) == 3);
-    assert(mat.sum(1,0, 1,1) == 8);
+    assert(mat.sum(0,1, 1,1) == 1);
+    assert(mat.sum(0,1, 2,1) == 6);
+
+    assert(mat.sum(1,0, 1,1) == 3);
+    assert(mat.sum(1,0, 1,2) == 8);
 
     assert(mat.sum(1,1, 1,1) == 5);
+
+    // ------------------------------------------------------------
+    mat.resize(2, 3);
+    assert(mat.height() == 2);
+    assert(mat.width() == 3);
+
+    mat.at(0, 0) = 1; mat.at(0, 1) = 2; mat.at(0, 2) = 3;
+    mat.at(1, 0) = 4; mat.at(1, 1) = 5; mat.at(1, 2) = 6;
+
+    mat.build();
+
+
+    outl("---------------");
+    outl(mat);
+    cout.flush();
+
+    assert(mat.sum({0, 0}, {1, 1}) == 1);
+    assert(mat.sum({0, 1}, {1, 2}) == 2);
+    assert(mat.sum({1, 2}, {2, 3}) == 6);
+
+    assert(mat.sum({0, 0}, {2, 3}) == 21);
+    assert(mat.sum({0, 0}, {1, 3}) == 6);
+    assert(mat.sum({1, 0}, {2, 3}) == 15);
+    assert(mat.sum({0, 0}, {2, 2}) == 12);
+
+    outl(__func__, ": AC");
 } // }}}
+
 
 signed main()
 {
     cin.tie(nullptr); ios::sync_with_stdio(false);
 
-    Ruisekiwa rui {{ 3, 1, 4, 1, 5 }};
-    trace(/* before */ rui);
+    test_Ruisekiwa1D();
 
-    rui.build();
-    trace(/* after */ rui);
+    test_Ruisekiwa2D();
 
     return 0;
 }
