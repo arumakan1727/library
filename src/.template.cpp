c94a805fe7f11fca2a4d6b42be89d8a6eb1b3a51
ydk1727 <ydk_nk@outlook.jp>
2019-08-18 16:03:59 +0900

    fix template

--- src/.template.cpp
+++ src/.template.cpp
@@ -2,19 +2,17 @@
 // Begin Header {{{
 #define let             const auto
 #define all(x)          (x).begin(), (x).end()
-#define SIZE(x)         static_cast<i64>(x.size())
 #define rep(i, n)       for (i64 i = 0, i##_limit = (n); i < i##_limit; ++i)
 #define reps(i, s, t)   for (i64 i = (s), i##_limit = (t); i <= i##_limit; ++i)
 #define repr(i, s, t)   for (i64 i = (s), i##_limit = (t); i >= i##_limit; --i)
-#define VAR(Type, ...)  Type __VA_ARGS__; input(__VA_ARGS__)
-#define VEC(Type, varName, sz)   vector<Type> varName((sz) + 1)
-#define lowerBound(...)              _lowerBound(__VA_ARGS__)
-#define upperBound(...)              _upperBound(__VA_ARGS__)
-#define _lowerBound(begin, end, ...) (lower_bound((begin), (end), __VA_ARGS__) - (begin))
-#define _upperBound(begin, end, ...) (upper_bound((begin), (end), __VA_ARGS__) - (begin))
+#define var(Type, ...)  Type __VA_ARGS__; input(__VA_ARGS__)
+#define lowerBound(...)                 _lowerBound(__VA_ARGS__)
+#define upperBound(...)                 _upperBound(__VA_ARGS__)
+#define _lowerBound(begin, end, ...)    (lower_bound((begin), (end), __VA_ARGS__) - (begin))
+#define _upperBound(begin, end, ...)    (upper_bound((begin), (end), __VA_ARGS__) - (begin))
 
 #ifdef DBG
-#define trace(...) _internal::_trace(#__VA_ARGS__, __VA_ARGS__)
+#define trace(...) _trace(#__VA_ARGS__, __VA_ARGS__)
 #else
 #define trace(...)
 #endif
@@ -26,40 +24,20 @@ template<class T> using MaxHeap = priority_queue<T>;
 template<class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 template<class T, class U>inline bool chmax(T &a, const U &b){return b>a && (a=b, true);}
 template<class T, class U>inline bool chmin(T &a, const U &b){return b<a && (a=b, true);}
-inline i64  sigma(i64 n)                    { return n * (n + 1) >> 1; }
+inline i64  sigma(i64 n)                    { return (n * (n + 1) >> 1); }
 inline i64  divup(i64 a, i64 b)             { return (a + b - 1) / b; }
-inline bool inner(i64 a, i64 low, i64 high) { return (low <= a && a < high); }
-inline string to_string(char c)             { return string(1,c); }
+inline bool inner(i64 a, i64 low, i64 high) { return (low <= a && a <= high); }
+inline string to_string(char c)             { return string(1, c); }
 constexpr int INF  = 0x3f3f3f3f;
 constexpr i64 LINF = 0x3f3f3f3f3f3f3f3fLL;
 
-// Input, Output {{{
-namespace _internal { // {{{
-    template<size_t> struct _Uint{};
-    template<class Tuple>
-        ostream& _print_tuple(ostream &out, const Tuple &t, _Uint<1>) {
-            return out << std::get< std::tuple_size<Tuple>::value - 1 >(t);
-        }
-    template<class Tuple, size_t Index>
-        ostream& _print_tuple(ostream &out, const Tuple &t, _Uint<Index>) {
-            out << std::get< std::tuple_size<Tuple>::value - Index >(t) << ' ';
-            return _print_tuple(out, t, _Uint<Index-1>());
-        }
-    template <class T>
-        void _trace(const char *s, T&& x) {
-            clog << '{';
-            while(*s != '\0') clog << *(s++);
-            clog << ":" << setw(3) << x << '}' << endl;
-        }
-    template <class Head, class... Tail>
-        void _trace(const char *s, Head&& head, Tail&&... tail) {
-            clog << '{';
-            while(*s != ',') clog << *(s++);
-            clog << ":" << setw(3) << head << "}, ";
-            for (++s; !isgraph(*s); ++s);
-            _trace(s, std::forward<Tail>(tail)...);
-        }
-} // }}}
+template<class T>
+vector<T> makeVec(size_t sz) { return vector<T>(sz); }
+
+template<class T, class... Args>
+auto makeVec(size_t sz, Args... args) {
+    return vector<decltype(makeVec<T>(args...))>(sz, makeVec<T>(args...));
+}
 
 template<class T>
 inline void input(T &x) { cin >> x; }
@@ -67,34 +45,44 @@ inline void input(T &x) { cin >> x; }
 template<class Head, class... Tail>
 inline void input(Head &head, Tail&... tail) { cin >> head; input(tail...); }
 
-template<class... Args>
-inline void outl(Args&&... args) { cout << make_tuple(std::forward<Args>(args)...) << "\n"; }
+inline void print() { cout << "\n"; }
+
+template<class Head, class... Tail>
+inline void print(Head &&head, Tail&&... tail) {
+    cout << head;
+    if (sizeof...(tail)) cout << ' ';
+    print(forward<Tail>(tail)...);
+}
 
 template<class T>
 ostream& operator<< (ostream &out, const vector<T> &vec) {
-    static const char *delim[] = { " ", "" };
-    rep(i, vec.size()) out << vec[i] << delim[i+1 == vec.size()];
+    static constexpr const char *delim[] = { " ", "" };
+    for (const auto &e : vec) out << e << delim[&e == &vec.back()];
     return out;
 }
 
-template<class T, class U>
-ostream& operator<< (ostream &out, const pair<T, U> &p) {
-    return out << p.first << ' ' << p.second;
-}
-
-template<class... Types>
-ostream& operator<< (ostream &out, const tuple<Types...> &t) {
-    return _internal::_print_tuple(out, t, _internal::_Uint<sizeof...(Types)>());
-}
-
-template<class Itr>
-ostream& print(Itr begin, Itr end, ostream &out = cout, const string &separator = " ") {
-    const auto rbegin = std::prev(end);
-    for (; begin != rbegin; ++begin) out << *begin << separator;
-    out << *rbegin;
+template<class T>
+ostream& operator<< (ostream &out, const vector<vector<T>> &mat) {
+    static constexpr const char *tail[] = { "\n", "" };
+    for (const auto &row : mat) out << row << tail[&row == &mat.back()];
     return out;
-} // }}}
+}
 
+template <class T>
+void _trace(const char *s, T&& x) {
+    clog << '{';
+    while(*s != '\0') clog << *(s++);
+    clog << ":" << setw(3) << x << '}' << endl;
+}
+
+template <class Head, class... Tail>
+void _trace(const char *s, Head&& head, Tail&&... tail) {
+    clog << '{';
+    while(*s != ',') clog << *(s++);
+    clog << ":" << setw(3) << head << "}, ";
+    for (++s; !isgraph(*s); ++s);
+    _trace(s, std::forward<Tail>(tail)...);
+}
 // }}} End Header
 
 signed main()
