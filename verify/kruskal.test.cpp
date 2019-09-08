228f5ac136d2d2bd13950be9c95ec72e51306281
ydk1727 <ydk_nk@outlook.jp>
2019-09-08 17:45:16 +0900

    :heavy_check_mark: kruskal verify OK

--- verify/kruskal.test.cpp
+++ verify/kruskal.test.cpp
@@ -1,5 +1,5 @@
 #define PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/2/GRL_2_A
-// Verified On: 2019-08-13
+// Verified On: 2019-09-08
 
 #include "../src/bits/stdc++.h"
 #include "../src/commonHeader.hpp"
@@ -13,7 +13,7 @@ signed main()
     int V, E;
     cin >> V >> E;
 
-    Edges es(E);
+    vector<Edge> es(E);
     for (int i = 0; i < E; ++i) {
         int s, t, w;
         cin >> s >> t >> w;
